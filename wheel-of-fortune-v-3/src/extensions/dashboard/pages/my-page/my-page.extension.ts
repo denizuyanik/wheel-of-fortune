40ac6062f4952170af9d7d4a50b98861a1bb4b97
Deniz Uyanık <denizuyanik@gmail.com>
2026-08-19 16:30:48 +0300

    feat(v3): bind and deploy clean official release v1.0.0 for App ID 1a5328a6-a435-4bf4-9e85-036480ce73ad

---
+++ wheel-of-fortune-v-3/src/extensions/dashboard/pages/my-page/my-page.extension.ts
@@ -0,0 +1,9 @@
+import { extensions } from '@wix/astro/builders';
+
+export default extensions.dashboardPage({
+  id: 'fd3ca303-c475-4eb6-972c-d6e6e600ce9a',
+  title: 'My Page',
+  routePath: '',
+  component: './extensions/dashboard/pages/my-page/my-page.tsx',
+  fullPage: false,
+});
